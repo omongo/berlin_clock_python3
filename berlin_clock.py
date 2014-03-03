220b4f6e216ac74b8d27a8a500467ce3fa66e917
Chanathip Srithanrat <omongo@gmail.com>
2014-03-04 00:51:51 +0700

    Refactor the codes

--- berlin_clock.py
+++ berlin_clock.py
@@ -1,33 +1,42 @@
 class BerlinClock:
 
+    @classmethod
+    def from_str(cls, time_str):
+        return cls(*time_str.split(':'))
+
     def __init__(self, hour=None, minute=None, second=None):
         self.hour = int(hour)
         self.minute = int(minute)
         self.second = int(second)
 
-    @classmethod
-    def from_str(cls, time_str):
-        return cls(*time_str.split(':'))
+    def __str__(self):
+        second_row = self._second_row()
+        hour_rows = self._hour_rows()
+        minute_rows = self._minute_rows()
+        return '{}\n{}\n{}'.format(second_row, hour_rows, minute_rows)
 
-    def display(self):
-        return '{} {} {}'.format(self._sec_pos(), self._hour_pos(), self._min_pos())
-
-    def _sec_pos(self):
+    def _second_row(self):
         return 'Y' if self.second % 2 else 'O'
 
-    def _hour_pos(self):
-        hour1 = self._pattern('R', self.hour // 5, 4)
-        hour2 = self._pattern('R', self.hour % 5, 4)
-        return '{} {}'.format(hour1, hour2)
+    def _hour_rows(self):
+        hour_1 = self._pattern('R', self.hour // 5, 4)
+        hour_2 = self._pattern('R', self.hour % 5, 4)
+        return '{}\n{}'.format(hour_1, hour_2)
 
-    def _min_pos(self):
+    def _minute_rows(self):
         minute_div_five = self.minute // 5
-        minute_mod_five = self.minute % 5
-        min1 = 'O' * (11 - minute_div_five)
+        min_1 = self._pattern('', minute_div_five, 11)
         for i in reversed(range(minute_div_five)):
-            min1 = ('R' if i % 3 == 2 else 'Y') + min1
-        min2 = self._pattern('Y', self.minute % 5, 4)
-        return '{} {}'.format(min1, min2)
+            min_1 = ('R' if i % 3 == 2 else 'Y') + min_1
+        min_2 = self._pattern('Y', self.minute % 5, 4)
+        return '{}\n{}'.format(min_1, min_2)
 
     def _pattern(self, *args):
         return args[0] * args[1] + 'O' * (args[2] - args[1])
+
+if __name__ == '__main__':
+    import time
+    current_time = time.strftime('%H:%M:%S')
+    berlin_clock = BerlinClock.from_str(time.strftime('%H:%M:%S'))
+    print(current_time)
+    print(berlin_clock)
--- test_berlin_clock.py
+++ test_berlin_clock.py
@@ -5,72 +5,72 @@ from berlin_clock import BerlinClock
 class testBerlinClock(unittest.TestCase):
     def test_000000(self):
         berlin_clock = BerlinClock.from_str('00:00:00')
-        expected = 'O OOOO OOOO OOOOOOOOOOO OOOO'
+        expected = 'O\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_000001(self):
         berlin_clock = BerlinClock.from_str('00:00:01')
-        expected = 'Y OOOO OOOO OOOOOOOOOOO OOOO'
+        expected = 'Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_050001(self):
         berlin_clock = BerlinClock.from_str('05:00:01')
-        expected = 'Y ROOO OOOO OOOOOOOOOOO OOOO'
+        expected = 'Y\nROOO\nOOOO\nOOOOOOOOOOO\nOOOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_100001(self):
         berlin_clock = BerlinClock.from_str('10:00:01')
-        expected = 'Y RROO OOOO OOOOOOOOOOO OOOO'
+        expected = 'Y\nRROO\nOOOO\nOOOOOOOOOOO\nOOOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_110001(self):
         berlin_clock = BerlinClock.from_str('11:00:01')
-        expected = 'Y RROO ROOO OOOOOOOOOOO OOOO'
+        expected = 'Y\nRROO\nROOO\nOOOOOOOOOOO\nOOOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_120001(self):
         berlin_clock = BerlinClock.from_str('12:00:01')
-        expected = 'Y RROO RROO OOOOOOOOOOO OOOO'
+        expected = 'Y\nRROO\nRROO\nOOOOOOOOOOO\nOOOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_120101(self):
         berlin_clock = BerlinClock.from_str('12:01:01')
-        expected = 'Y RROO RROO OOOOOOOOOOO YOOO'
+        expected = 'Y\nRROO\nRROO\nOOOOOOOOOOO\nYOOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_120201(self):
         berlin_clock = BerlinClock.from_str('12:02:01')
-        expected = 'Y RROO RROO OOOOOOOOOOO YYOO'
+        expected = 'Y\nRROO\nRROO\nOOOOOOOOOOO\nYYOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_120701(self):
         berlin_clock = BerlinClock.from_str('12:07:01')
-        expected = 'Y RROO RROO YOOOOOOOOOO YYOO'
+        expected = 'Y\nRROO\nRROO\nYOOOOOOOOOO\nYYOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_121201(self):
         berlin_clock = BerlinClock.from_str('12:12:01')
-        expected = 'Y RROO RROO YYOOOOOOOOO YYOO'
+        expected = 'Y\nRROO\nRROO\nYYOOOOOOOOO\nYYOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_121701(self):
         berlin_clock = BerlinClock.from_str('12:17:01')
-        expected = 'Y RROO RROO YYROOOOOOOO YYOO'
+        expected = 'Y\nRROO\nRROO\nYYROOOOOOOO\nYYOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
 
     def test_123201(self):
         berlin_clock = BerlinClock.from_str('12:32:01')
-        expected = 'Y RROO RROO YYRYYROOOOO YYOO'
+        expected = 'Y\nRROO\nRROO\nYYRYYROOOOO\nYYOO'
         actual = berlin_clock.display()
         self.assertEqual(expected, actual)
